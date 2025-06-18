150e7fb83f6a889015e9ca0f2f338199d4457540
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-06-18 20:07:06 +0700

    Shoot Animation

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,25 +3,37 @@ using StarterAssets;
 
 public class Weapon : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private Animator animator;
     [SerializeField] private ParticleSystem muzzleFlash;
     [SerializeField] private int weaponDamage = 50;
     StarterAssetsInputs starterAssetsInputs;
+    private const string SHOOT_STRING = "Shoot";
 
     private void Awake()
     {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
+
+    private void Start()
+    {
+        muzzleFlash.Stop();
     }
     private void Update()
     {
         if (starterAssetsInputs.shoot)
         {
             Shoot();
+            
         }
     }
 
     private void Shoot()
     {
         muzzleFlash.Play();
+        animator.Play(SHOOT_STRING,0,0f);
+        starterAssetsInputs.shoot = false;
+
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
@@ -31,7 +43,6 @@ public class Weapon : MonoBehaviour
                 Debug.Log("Hit " + hit.collider.name);
                 health.Damage(weaponDamage); 
             }
-            starterAssetsInputs.ShootInput(false);
         }
     }
 
